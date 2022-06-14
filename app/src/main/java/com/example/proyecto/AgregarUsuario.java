b4fa0dcad3049050a73e8fd96f572db763708c19
Mariana Ramírez Llamas <78526046+Mariana914Ram@users.noreply.github.com>
2022-06-14 15:32:27 -0500

    Multiselect de almacenes

--- app/src/main/java/com/example/proyecto/AgregarUsuario.java
+++ app/src/main/java/com/example/proyecto/AgregarUsuario.java
@@ -1,13 +1,14 @@
-package com.example.proyecto;
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -16,29 +17,143 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 
-public class Registrar extends AppCompatActivity {
+public class AgregarUsuario extends AppCompatActivity {
 
     EditText nombre, apellidos, correo, password, repetPassword;
+    CheckBox tipoA, tipoB;
+    TextView tvAlmacen;
+    boolean[] selectAlmacen;
+    ArrayList<Integer> almacenList = new ArrayList<>();
+    String[] almacenArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registrar);
+        setContentView(R.layout.activity_agregar_usuario);
 
         nombre = (EditText) findViewById(R.id.et_nombre);
         apellidos = (EditText) findViewById(R.id.et_apellidos);
         correo = (EditText) findViewById(R.id.et_correoGuardar);
         password = (EditText) findViewById(R.id.et_contraGuardar);
         repetPassword = (EditText) findViewById(R.id.et_confirmarContra);
+        tipoA = (CheckBox) findViewById(R.id.cbxAdmin);
+        tipoB = (CheckBox) findViewById(R.id.cbxNormal);
+        tvAlmacen = (TextView) findViewById(R.id.tvAlmacen);
 
 
+        String texto = abrirArchivo("archivoAlmacenes.txt");
+        Toast.makeText(this, texto, Toast.LENGTH_SHORT).show();
+
+        int contador = 0;
+        String cadena = "";
+        if(!texto.equals("")){
+            String[] almacen = texto.split("\n\n");
+            for(int i=0; i<almacen.length; i++){
+                String[] parteAlmacen = almacen[i].split("\n");
+                String nombre_temp = "";
+                for(int j=0; j<parteAlmacen.length; j++) {
+
+                    if (parteAlmacen[j].contains("nombre: ")) {
+                        nombre_temp = parteAlmacen[j];
+                        nombre_temp = nombre_temp.replace("nombre: ", "");
+
+                        cadena = cadena + nombre_temp;
+                        if(i+1 != almacen.length){
+                            cadena = cadena + " ";
+                        }
+                        contador++;
+                    }
+                }
+            }
+        }
+
+        almacenArray = cadena.split(" ");
+        Toast.makeText(this, almacenArray[0]+"", Toast.LENGTH_SHORT).show();
+
+
+        selectAlmacen = new boolean[almacenArray.length];
+        tvAlmacen.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                AlertDialog.Builder builder = new AlertDialog.Builder(
+                        AgregarUsuario.this
+                );
+
+                builder.setTitle("Selecciona los almacenes");
+                builder.setCancelable(false);
+
+                builder.setMultiChoiceItems(almacenArray, selectAlmacen, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                        if(b){
+                            almacenList.add(i);
+                            Collections.sort(almacenList);
+                        }
+                        else{
+                            almacenList.remove(Integer.valueOf(i));;
+                        }
+                    }
+                });
+
+
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        StringBuilder stringBuilder = new StringBuilder();
+
+                        for(int j=0; j<almacenList.size(); j++){
+                            stringBuilder.append(almacenArray[almacenList.get(j)]);
+
+                            if(j != almacenList.size()-1){
+                                stringBuilder.append(", ");
+                            }
+                        }
+                        tvAlmacen.setText(stringBuilder.toString());
+                    }
+                });
+
+
+                builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+
+
+                builder.setNeutralButton("Limpiar todo", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        for(int j=0; j<selectAlmacen.length; j++){
+                            selectAlmacen[j] = false;
+                            almacenList.clear();
+                            tvAlmacen.setText("");
+                        }
+                    }
+                });
+
+
+                builder.show();
+            }
+        });
     }
+
+
+
+
 
 
     public void registrarUsuario(View view){
         boolean verificar = true;
 
+        if(!tipoA.isChecked() && !tipoB.isChecked()){
+            Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "Debe escoger un tipo de usuario", Snackbar.LENGTH_LONG);
+            snackbar.show();
+            verificar = false;
+        }
         if(nombre.getText().toString().isEmpty()){
             nombre.setError("Campo requerido");
             verificar = false;
@@ -99,22 +214,20 @@ public class Registrar extends AppCompatActivity {
                 snackbar.show();
 
             }
-
-            String tipo = "normal";
-
+            
             texto = texto + "correo: " + correo.getText().toString().trim() + "\n";
             texto = texto + "contrasena: " + password.getText().toString().trim() + "\n";
             texto = texto + "nombre: " + nombre.getText().toString().trim() + "\n";
             texto = texto + "apellidos: " + apellidos.getText().toString().trim() + "\n";
-            texto = texto + "tipo: normal" + "\n";
-
+            if(tipoA.isChecked()){
+                texto = texto + "tipo: admin" + "\n";
+            }else{
+                texto = texto + "tipo: normal" + "\n";
+            }
             guardarArchivo(texto);
 
 
-            IngresoAplicacion usuario = new IngresoAplicacion(id+"", correo.getText().toString().trim(), password.getText().toString().trim(), nombre.getText().toString().trim(), apellidos.getText().toString().trim(), tipo, true);
-
-            guardarPreferencias(usuario);
-            Intent intent = new Intent(Registrar.this, MainActivity.class);
+            Intent intent = new Intent(AgregarUsuario.this, vista_usuario.class);
             startActivity(intent);
             finish();
         }
@@ -129,7 +242,7 @@ public class Registrar extends AppCompatActivity {
             archivo.flush();
             archivo.close();
         } catch (IOException e){
-            Toast.makeText(Registrar.this, "Error al escribir en el archivo", Toast.LENGTH_LONG).show();
+            Toast.makeText(AgregarUsuario.this, "Error al escribir en el archivo", Toast.LENGTH_LONG).show();
         }
     }
 
@@ -181,20 +294,5 @@ public class Registrar extends AppCompatActivity {
         }
         return false;
     }
-
-
-
-
-    private void guardarPreferencias(IngresoAplicacion usr){
-        SharedPreferences preferences = getSharedPreferences("user.dat", MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString("id", usr.getId());
-        editor.putString("correo", usr.getCorreo());
-        editor.putString("contrasena", usr.getContrasena());
-        editor.putString("nombre", usr.getNombre());
-        editor.putString("apellidos", usr.getApellidos());
-        editor.putString("tipo", usr.getTipo());
-        editor.putBoolean("registrado", usr.isRegistrado());
-        editor.apply();
-    }
+    
 }
